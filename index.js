576b6087262f421c35632bbca7b6eb67fef12435
cseswapon <cseswaponsaha@gmail.com>
2021-11-13 22:43:05 +0600

    order deleted

--- index.js
+++ index.js
@@ -79,6 +79,12 @@ async function run() {
       );
       res.json(result);
     });
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(filter);
+      res.send(result);
+    });
     // delete order
     app.delete("/order/:id", async (req, res) => {
       const id = req.params.id;
