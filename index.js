26c30c10357d50b458e04ccda8ea104f65e6f2ba
cseswapon <cseswaponsaha@gmail.com>
2022-01-10 13:15:54 +0600

    [ADD] - JWT Token added

--- index.js
+++ index.js
@@ -5,18 +5,35 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase-adminsdk-fn8u6-.json");
 // middle war
 
 app.use(cors());
 app.use(express.json());
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ifldk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+// verify
+async function verification(req, res, next) {
+  if (req?.headers?.authorization?.startsWith("Bearer ")) {
+    const idToken = req?.headers?.authorization?.split(" ")[1];
+    try {
+      const decoder = await admin.auth().verifyIdToken(idToken);
+      req.decoderUser = decoder.email;
+    } catch {
+      res.json({ message: "error" });
+    }
+  }
+  next();
+}
 async function run() {
   try {
     await client.connect();
@@ -26,7 +43,6 @@ async function run() {
     const productReview = database.collection("Review");
     const orderCollection = database.collection("Order");
     const usersCollection = database.collection("Users");
-
     // product GET
     app.get("/products", async (req, res) => {
       const result = await productCollection.find({}).toArray();
@@ -46,12 +62,13 @@ async function run() {
       const result = await productCollection.deleteOne(filter);
       res.json(result);
     });
+    // add product
     app.post("/products", async (req, res) => {
       const data = req.body;
       const result = await productCollection.insertOne(data);
       res.json(result);
     });
-    // add product
+    // add order
     app.post("/order", async (req, res) => {
       const data = req.body;
       const result = await orderCollection.insertOne(data);
@@ -61,6 +78,16 @@ async function run() {
     app.get("/order", async (req, res) => {
       const result = await orderCollection.find({}).toArray();
       res.send(result);
+    });
+    app.get("/orders", verification, async (req, res) => {
+      const email = req.query.email;
+      if (req.decoderUser === email) {
+        const filter = { email };
+        const result = await orderCollection.find(filter).toArray();
+        res.send(result);
+      } else {
+        res.status(401).json({ message: "not yet" });
+      }
     });
     // put order
     app.put("/order/:id", async (req, res) => {
